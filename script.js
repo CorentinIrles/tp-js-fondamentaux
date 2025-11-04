7792718fa5080c876ca8b4db0a34251bad875ba2
CorentinIrles <corentin.irles@icloud.com>
2025-11-04 08:52:31 +0000

    feat: Maitrise des variables et conditions

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Laboratoire prêt !");
+const nom = "Corentin";
+let age = 18;
+console.log(age)
+const motDePasseAttendu = "secret123";
+let motDePasseSaisi = prompt("Veuillez entrer le mot de passe :");
+
+if ("Comparaison entre motDePasseUtilisateur et motDePasseAttendu") {
+    console.log("Accès autorisé. Bienvenue !");
+} else if ("Vérification de la longueur du mot de passe (< 8 caractères)") {
+    console.log("Accès refusé. Votre mot de passe est trop court (8 caractères min).");
+} else {
+    console.log("Accès refusé. Mot de passe incorrect.");
+}
